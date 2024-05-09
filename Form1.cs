5881174f915dead5e6ea788b420e3295e1344ba3
lanasamnbhd <120123793+lanasamnbhd@users.noreply.github.com>
2024-05-09 21:45:00 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace KitapMagazasiOtomasyonu1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            s
+        }
+    }
+}
